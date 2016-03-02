e3ddd482c9e0819ed2401ce1d40598a75f5e194b
johan411 <abhijithsethuraj4@gmail.com>
2016-03-02 19:18:02 +0530

    abhijith

---
+++ projectile/Assets/Scripts/TargetController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetController : MonoBehaviour {
+
+	public float target_speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if ( Input.GetKey(KeyCode(
+	
+	}
+
+	void FixedUpdate (){
+
+		if (Input.GetKey ("left")) {
+
+			transform.Translate (-(target_speed), 0, 0);
+
+		}
+
+		if (Input.GetKey ("right")) {
+
+			transform.Translate (target_speed, 0, 0);
+
+		}
+
+		if (Input.GetKey ("e")) {
+
+
+
+		}
+	}
+}
---
+++ projectile/Assets/Scripts/WheelController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelController : MonoBehaviour {
+
+	public Rigidbody2D wheel;
+
+	public Collider2D ball;
+
+	// Use this for initialization
+	void Start () {
+
+		wheel = this.GetComponent<Rigidbody2D> ();
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D ball){
+
+		if (ball.tag == "ball") {
+
+			wheel.WakeUp();
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
